2b59c39a9e66fd8c75ae3ff7c7d319586a9dbcc5
Camilo Andrés Díaz Gómez <camiloandres2288@hotmail.com>
2022-04-11 21:13:47 -0500

    Actualización final

--- 02. Desarrollo/Proyecto Integrador/Viejo Niote/Documentos/user and db.sql
+++
@@ -1,3 +0,0 @@
-create database if not exists ingusbbo_piico;
-create user 'ingusbbo_piico'@'%' identified by 'Clt28$2}C]xo';
-grant all privileges on ingusbbo_piico.* to ingusbbo_piico with grant option;
